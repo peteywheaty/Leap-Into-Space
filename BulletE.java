924095959c928ec4ed1ed1f80ea90ac8fb24f596
Peter Cheng <petercheng777@hotmail.com>
2015-01-17 17:26:16 -0500

    Create BulletE.java

---
+++ BulletE.java
@@ -0,0 +1,10 @@
+package com.leapmotionsi;
+
+import javax.swing.ImageIcon;
+
+public class BulletE extends Sprite {
+	
+	public BulletE() {
+        setImage((new ImageIcon(this.getClass().getResource("bullet2.png"))).getImage());
+	}
+}
